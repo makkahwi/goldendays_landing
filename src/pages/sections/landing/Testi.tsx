f3a67e0c0389be9bf49345ada121bd1b93682223
makkahwi <suhaibahmadai@hotmail.com>
2023-03-29 23:30:24 +0300

    Update Office Location Section

--- src/pages/sections/landing/Testi.tsx
+++ src/pages/sections/landing/Testi.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Link from "next/link";
-import Revcard from "@/components/Revcard";
 import myImage from "@/../public/21.jpg";
+import Revcard from "@/components/Revcard";
+import Link from "next/link";
+import React from "react";
 
 const Testi = () => {
   return (
@@ -10,74 +10,34 @@ const Testi = () => {
       data-testid="review"
       className="flex flex-col xl:container"
     >
-      <div className="min-h-[8rem]"></div>
+      <div className="min-h-[8rem]" />
+
       <div className="flex flex-wrap justify-center px-8 gap-y-10 min-h-[80vh] md:min-h-[50vh] gap-x-6">
         <div className="flex flex-col justify-center items-center gap-y-6 lmd:w-[47%] lmd:order-2">
           <div className="flex flex-col items-center gap-y-2">
-            <h2 className="text-4xl font-semibold text-center text-emerald-900 md:font-bold lg:text-5xl">
-              Help some travelers?
-            </h2>
-            <p className="text-xl font-semibold text-gray-700 md:text-2xl lg:text-3xl">
-              Write a review.
-            </p>
+            <h3 className="text-3xl font-semibold text-center text-emerald-900 md:font-bold lg:text-3xl">
+              Visit US @ Our Offices
+            </h3>
           </div>
+
           <p className="text-center font-light max-w-md lg:text-lg">
-            Share your experience to help over 5 million travelers to decide
-            their journey in Travel
+            Sun - Thu | 9am - 6pm
           </p>
-          <div className="flex items-center gap-x-4">
-            <Link
-              href=""
-              className="px-4 py-2 border-2 border-gray-700 bg-gray-700 text-gray-100 font-semibold w-fit lg:px-6 lg:py-3"
-            >
-              Write a Review
-            </Link>
-            <Link
-              href=""
-              className="px-4 py-2 border-2 border-gray-700 text-gray-700 font-semibold w-fit lg:px-6 lg:py-3"
-            >
-              Read Reviews
-            </Link>
-          </div>
+
+          <p className="text-center font-light max-w-md lg:text-lg">
+            No.121, Al-Arab St., Amman, Jordan
+          </p>
         </div>
 
         <div className="flex justify-center w-full lmd:w-[47%] lmd:order-1 h-[30rem] items-center bg-[#fefefe]">
           <div className="relative h-[20rem] w-[27rem] lg:w-[33rem]">
-            <div className="w-full p-5 card-shadow max-w-[21rem] lg:max-w-[25rem] bg-white z-30 absolute top-0 right-0">
-              <Revcard
-                name="David Johns"
-                job="Graphic Designer"
-                company="Apple Company"
-                comment={myImage}
-                image="/pp2.jpg"
-                headline="It's a breathtaking journey"
-              />
-            </div>
-            <div className="w-full p-5 card-shadow max-w-[21rem] lg:max-w-[25rem] bg-white absolute z-10 -top-[15%] right-1/4">
-              <Revcard
-                name="Renka Nawasena"
-                job="Software Developer"
-                company="Google Company"
-                comment="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-              placeat quibusdam odio nesciunt! Debitis, ducimus blanditiis! Nulla
-              veniam quae adipisci."
-                image="/pp1.jpg"
-                headline="Wish i discover sooner"
-              />
-            </div>
-
-            <div className="w-full p-5 card-shadow max-w-[21rem] lg:max-w-[25rem] bg-white absolute z-20 top-[45%] left-[5%]">
-              <Revcard
-                name="Alexander William"
-                job="Mechanical Engineer"
-                company="Chevron Company"
-                comment="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-                placeat quibusdam odio nesciunt! Debitis, ducimus blanditiis! Nulla
-                veniam quae adipisci."
-                image="/pp2.jpg"
-                headline="Worth to try!"
-              />
-            </div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d928.6538264128172!2d35.892212942653046!3d32.04730683521102!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151c9fc3b393313d%3A0x39f3efca2a62bf5f!2z2KfZhNin2YrYp9mFINin2YTYsNmH2KjZitipINmE2YTYs9mK2KfYrdipINmI2KfZhNiz2YHYsSDZiNin2YTYrdisINmI2KfZhNi52YXYsdip!5e0!3m2!1sen!2sus!4v1680121425382!5m2!1sen!2sus"
+              width="100%"
+              height="400"
+              style={{ border: 0 }}
+              loading="lazy"
+            />
           </div>
         </div>
       </div>
