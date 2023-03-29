548cacad6e8f90aff43788908af37293671e8bb9
makkahwi <suhaibahmadai@hotmail.com>
2023-03-29 23:22:33 +0300

    Update Services Section Content

--- src/components/Picard.tsx
+++ src/components/Picard.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { MdFolder } from "react-icons/md";
+import React from "react";
 import { BsBookmarkFill } from "react-icons/bs";
+import { MdFolder } from "react-icons/md";
 
 interface cardProps {
   src: string;
@@ -21,18 +21,6 @@ const Picard = ({ src, title, desc }: cardProps) => {
           height={632}
           className="group-hover:scale-110 group-hover:brightness-75 brig transition-all duration-300"
         />
-        <div className="absolute top-0 opacity-0 group-hover:opacity-100 w-full flex justify-between transition-opacity duration-300 p-4">
-          <Link
-            href=""
-            className="flex items-center gap-x-2 px-3 py-2 bg-gray-700 text-gray-100 rounded-3xl bg-opacity-70"
-          >
-            <MdFolder />
-            <p className="text-xs">Save</p>
-          </Link>
-          <button className="-translate-y-2 group-hover:translate-y-0 transition-all duration-300">
-            <BsBookmarkFill color="#ad9058" size={25} />
-          </button>
-        </div>
       </div>
       <div className="flex flex-col text-gray-800">
         <h3 className="font-semibold">{title}</h3>
